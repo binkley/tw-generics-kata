7d4029d85c28e0c36cd053919dba96594827f5cf
Brian Oxley (TW) <Brian.Oxley@wolterskluwer.com>
2017-08-31 16:40:09 -0400

    Skeleton exchange desk

---
+++ src/test/java/kata/ExchangeDeskTest.java
@@ -0,0 +1,17 @@
+package kata;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public final class ExchangeDeskTest {
+    private ExchangeDesk exchangeDesk;
+
+    @Before
+    public void setUp() throws Exception {
+        exchangeDesk = new ExchangeDesk();
+    }
+
+    @Test
+    public void firstTest() {
+    }
+}
